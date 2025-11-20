9a1243c8f709fd7c6fd869ec250d09967984633a
Kan <wu-kan@163.com>
2025-11-20 10:31:30 +0800

    动态加载配置使用更专业的写法

--- akshare_mcp/server.py
+++ akshare_mcp/server.py
@@ -1,5 +1,5 @@
+import importlib
 import inspect
-import pathlib
 import sys
 from functools import wraps
 from typing import Literal
@@ -39,19 +39,40 @@ def register(white_list, black_list, format: Literal["markdown", "csv", "json"])
         try:
             # TODO 参数能支持读取__doc__就好了
             mcp.tool()(output_format(func, format=format))
+            print(name, "注册成功")
         except Exception as e:
             print(name, "注册失败，需要调整函数参数")
 
 
+def import_module_from_path(module_name, file_path):
+    """
+    从指定文件路径导入模块
+
+    Args:
+        module_name (str): 要导入的模块名称（自定义）
+        file_path (str): Python 文件的绝对路径
+
+    Returns:
+        module: 导入的模块对象
+    """
+    # 根据文件路径创建模块规格
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    if spec is None:
+        raise ImportError(f"无法从路径 '{file_path}' 创建模块规格")
+    # 根据规格创建新的模块对象
+    module = importlib.util.module_from_spec(spec)
+    # 将模块添加到 sys.modules 全局字典中
+    sys.modules[module_name] = module
+    # 执行模块代码（加载模块）
+    spec.loader.exec_module(module)
+    return module
+
+
 def serve(format: Literal["markdown", "csv", "json"], transport: Literal["stdio", "sse", "streamable-http"], host: str, port: int, config: str = None):
     if config:
-        path = pathlib.Path(config)
-        sys.path.insert(0, str(path.parent))
-        cfg = __import__(path.stem, globals(), locals(), fromlist=[])
-        sys.path.pop(0)
-    else:
-        from akshare_mcp import config as cfg
+        import_module_from_path("akshare_mcp.config", config)
 
+    from akshare_mcp import config as cfg
     register(cfg.white_list, cfg.black_list, format=format)
 
     if transport == "stdio":
